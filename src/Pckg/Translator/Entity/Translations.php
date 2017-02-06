8f01deecf2d96350892bb29c23f983b8b48e3954
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-02-06 11:03:50 +0100

    Translations record

--- src/Pckg/Translator/Entity/Translations.php
+++ src/Pckg/Translator/Entity/Translations.php
@@ -2,11 +2,14 @@
 
 use Pckg\Database\Entity;
 use Pckg\Database\Entity\Extension\Translatable;
+use Pckg\Translator\Record\Translation;
 
 class Translations extends Entity
 {
 
     use Translatable;
+
+    protected $record = Translation::class;
 
     public function boot()
     {
---
+++ src/Pckg/Translator/Record/Record.php
@@ -0,0 +1,11 @@
+<?php namespace Pckg\Translator\Record;
+
+use Pckg\Database\Record;
+use Pckg\Translator\Entity\Translations;
+
+class Translation extends Record
+{
+
+    protected $entity = Translations::class;
+
+}
