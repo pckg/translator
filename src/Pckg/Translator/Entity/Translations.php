e691c0e9bbf9cf19df182a70daa5f784051a29a0
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-04-16 13:18:24 +0200

    Simplify extensions

--- src/Pckg/Translator/Entity/Translations.php
+++ src/Pckg/Translator/Entity/Translations.php
@@ -1,13 +1,10 @@
 <?php namespace Pckg\Translator\Entity;
 
 use Pckg\Database\Entity;
-use Pckg\Database\Entity\Extension\Translatable;
 use Pckg\Translator\Record\Translation;
 
 class Translations extends Entity
 {
-
-    use Translatable;
 
     protected $record = Translation::class;
 
