82a3e97048b8d275b7a213fb10d00bd408b2463e
Bojan Rajh <schtr4jh@schtr4jh.net>
2021-11-11 18:30:48 +0100

    Migration

---
+++ src/Pckg/Translator/Migration/CreateTranslationsTable.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pckg\Translator\Migration;
+
+use Pckg\Migration\Migration;
+
+class CreateTranslationsTable extends Migration
+{
+
+    public function up()
+    {
+        $translations = $this->table('translations');
+        $translations->slug();
+        $translations->varchar('title');
+
+        $translationsI18n = $this->translatable('translations');
+        $translationsI18n->text('value');
+
+        $this->save();
+    }
+}
